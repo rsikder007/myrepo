4242d4d4bb4de3d9d1ce6330be033324e9524bb2
rurs007 <108664745+rurs007@users.noreply.github.com>
2022-07-04 18:28:10 +0600

    Update repomod.py

--- myrepolib/repomod.py
+++ myrepolib/repomod.py
@@ -12,9 +12,9 @@ def print_name(name):
 
     return name + "-apple"
 
-#def fake_data():
- #   return {"one":1, "two":2}
+def fake_data():
+    return {"one":1, "two":2}
 
-#def call_web_service(url="http://localhost:5000/fakedata"):
-#    res = requests.get(url)
-#    return json.loads(res.content)
+def call_web_service(url="http://localhost:5000/fakedata"):
+    res = requests.get(url)
+    return json.loads(res.content)
