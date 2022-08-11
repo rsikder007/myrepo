d1fea7ada2876b25c0363b6679758fa8b833a126
rsikder007 <51390584+rsikder007@users.noreply.github.com>
2022-08-10 21:49:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import flask
 import pickle
 from flask import Flask, render_template, request
 
-app=Flask(__name__)@app.route(‘/’)
+app=Flask(__name__)
 
+
+@app.route(‘/’)
 def index():
     return flask.render_template(‘index.html’)
 
@@ -15,8 +17,9 @@ def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,4)
     loaded_model = pickle.load(open(“trained_model.h5”,”rb”))
     result = loaded_model.predict(to_predict)
-    return result[0]@app.route(‘/predict’,methods = [‘POST’])
+    return result[0]
 
+@app.route(‘/predict’,methods = [‘POST’])
 def result():
     if request.method == ‘POST’:
     to_predict_list = request.form.to_dict()
