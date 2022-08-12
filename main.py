21f20cdfba1b3f398daec20def472cc356118e97
rsikder007 <51390584+rsikder007@users.noreply.github.com>
2022-08-12 10:19:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template
+
+#Py packages
+import pandas as pd
+import numpy as np
+
+#ML packages
 
 App = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return 'Hello World!'
+def index():
+  return render_template("index.html")
+
+@app.route("/")
+def predict():
+  return render_template("predict.html")
 
 If __name__ == '__main__':
-  app.run(host='0.0.0.1',port=8080, debug=True)
+  app.run(host='127.0.0.1',port=8080, debug=True)
